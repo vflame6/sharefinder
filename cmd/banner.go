0a1629f18b97812fec27d76a3a6f86d8d141e2dc
vflame6 <vflame6x@gmail.com>
2026-04-15 01:42:07 +0500

    automate version string

--- cmd/banner.go
+++ cmd/banner.go
@@ -1,15 +1,26 @@
 package cmd
 
-import "fmt"
+import (
+	"fmt"
+	"runtime/debug"
+)
 
 // AUTHOR of the program
 const AUTHOR = "Maksim Radaev (@vflame6)"
 
 // VERSION should be linked to actual tag
-const VERSION = "v1.3.5"
+var VERSION = "dev"
 
 // BANNER format string
 const BANNER = "\n         __                    _____           __         \n   _____/ /_  ____ _________  / __(_)___  ____/ /__  _____\n  / ___/ __ \\/ __ `/ ___/ _ \\/ /_/ / __ \\/ __  / _ \\/ ___/\n (__  ) / / / /_/ / /  /  __/ __/ / / / / /_/ /  __/ /    \n/____/_/ /_/\\__,_/_/   \\___/_/ /_/_/ /_/\\__,_/\\___/_/ %s    \n                                                          \nMade by %s\n\n"
+
+func init() {
+	if VERSION == "dev" {
+		if info, ok := debug.ReadBuildInfo(); ok && info.Main.Version != "" && info.Main.Version != "(devel)" {
+			VERSION = info.Main.Version
+		}
+	}
+}
 
 // PrintBanner is a function to print program banner
 func PrintBanner() {
